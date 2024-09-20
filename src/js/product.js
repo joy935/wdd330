e700ffad2906c1dec2d49627beba696254a22c96
joy935 <tupaiv@gmail.com>
2024-09-20 12:52:44 +0200

    fixed add item to cart

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,13 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // setLocalStorage("so-cart", product); // fix this line because it overwrites the cart
+  // get the current cart items from local storage
+  const cartItems = getLocalStorage("so-cart") || [];
+  // add the new product to the cart items
+  cartItems.push(product);
+  // save the updated cart items to local storage
+  setLocalStorage("so-cart", cartItems);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
