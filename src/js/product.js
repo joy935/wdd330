2db680141c79dec7016de978da6eb4474742f05c
divineProjects <divinejigu@gmail.com>
2024-09-21 23:17:35 +0200

    updated W01 Individual Activity: Add Item to Cart Broken

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,19 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get the current cart items from local storage or initialize it empty array
+  const currentCartItems = getLocalStorage("so-cart") || [];
+
+  // Add new product 
+  currentCartItems.push(product); 
+
+  // Save the updated cart item back to local storage
+  setLocalStorage("so-cart", currentCartItems);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
