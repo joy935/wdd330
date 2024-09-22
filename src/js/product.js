da8ee585da88227caef0a9cb8c36a26b993457af
deanabriggs <145316150+deanabriggs@users.noreply.github.com>
2024-09-21 21:35:03 -0700

    Fix shopping cart in products.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,24 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+function addProductToCart(newProduct) {
+  // retrieve current cart info from local storage
+  const cart = getLocalStorage("so-cart") || [];
+
+  // check if the product is already in the cart
+  const exists = cart.some(product => product.Id === newProduct.Id);
+
+  // if the product isn't already in the cart, add it, otherwise do nothing
+  if (!exists) {
+    // add new item to the cart
+    cart.push(newProduct);
+    // update local storage with complete cart
+    setLocalStorage("so-cart", cart);
+  }
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
