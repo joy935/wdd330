0158f6abd5ec14af4600fd1170ec4c6cbc27bed6
joy935 <tupaiv@gmail.com>
2024-09-27 12:13:10 +0200

    removed add listener double

--- src/js/product.js
+++ src/js/product.js
@@ -10,11 +10,7 @@ product.init();
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  addToCart(product);
 }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
 
