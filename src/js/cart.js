40795149cdd1ef688ae99de602c00f8a7d95ea14
joy935 <tupaiv@gmail.com>
2024-09-30 09:15:32 +0200

    added cart fixes

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,7 +2,29 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
+
+  // if there are no items in the cart, display a message
+  if (!cartItems) {
+    document.querySelector(".product-list").innerHTML =
+      "<p>Your cart is empty</p>";
+    return;
+  }
+
+  // otherwise, display the cart items
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+
+  // calculate total price
+  let total = 0;
+  cartItems.forEach((item) => {
+    total += parseFloat(item.FinalPrice);
+  });
+
+  const cartFooter = document.querySelector(".cart-footer");
+  // show the cart footer
+  cartFooter.removeAttribute("hidden");
+  // add total price to the cart footer
+  cartFooter.innerHTML = `<p class="cart-total">Total: $${total}</p>`;
+
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
 
