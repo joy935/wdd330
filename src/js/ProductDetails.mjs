2d46cdba9887b9707bd786462363098514c07540
joy935 <tupaiv@gmail.com>
2024-10-04 17:56:45 +0200

    fix images

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -7,7 +7,7 @@ function productDetailsTemplate(product) {
         <section class="product-detail"> 
             <h3>${product.Brand.Name}</h3>
             <h2 class="divider">${product.NameWithoutBrand}</h2>
-            <img class="divider" src="${product.PrimaryLarge}" alt="${product.NameWithoutBrand}"/>        
+            <img class="divider" src="${product.Images.PrimaryLarge}" alt="${product.NameWithoutBrand}"/>        
             <p class="product-card__price">$${product.FinalPrice}</p>
             <p class="product__color">${product.Colors[0].ColorName}</p>
             <p class="product__description">${product.DescriptionHtmlSimple}</p>
