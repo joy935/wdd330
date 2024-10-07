38df6fb073ec40253bc41284875844c39e58b4c3
Nick Cherenkov <78023926+nicholas-28@users.noreply.github.com>
2024-10-07 13:25:29 +0400

    feat(alert): add customizable alert feature
    - Added Alert class to load and display alerts from JSON file.
    - Implemented CSS styling for better alert appearance.
    - Integrated alert feature into main.js.

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,10 @@
 import { loadHeaderFooter } from "./utils.mjs";
+import Alert from "./alert.js";
+
 
 loadHeaderFooter();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const alertInstance = new Alert();
+  alertInstance.loadAlerts();
+});
