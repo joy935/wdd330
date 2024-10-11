104c4cf4a73ec2218767db495405e42b7eb60f9f
joy935 <tupaiv@gmail.com>
2024-10-11 20:11:53 +0200

    minor fixes

--- src/js/search.js
+++ src/js/search.js
@@ -2,21 +2,21 @@ const search = document.querySelector("#search");
 const searchButton = document.querySelector("#search-button");
 const clearButton = document.querySelector("#clear-button");
 
-searchButton.addEventListener("click", async (e) => {
-  e.preventDefault();
+searchButton.addEventListener("click", async(e) => {
+    e.preventDefault();
 
-  let searchValue = search.value;
-
-  if (searchValue && searchValue.length > 0) {
-    searchValue == searchValue.trim().toLowerCase();
-    // redirect to the products page with the search query
-    window.location.href = `products/index.html?search=${searchValue}`;
-  } else {
-    // do nothing
-  }
+    let searchValue = search.value;
+    
+    if (searchValue && searchValue.length > 0) {
+        searchValue == searchValue.trim().toLowerCase();
+        // redirect to the products page with the search query
+        window.location.href = `products/index.html?search=${searchValue}`;
+    } else {
+            // do nothing
+    }
 });
 
 clearButton.addEventListener("click", () => {
-  search.value = "";
-  search.focus();
+    search.value = "";
+    search.focus();
 });
