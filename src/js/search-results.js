b2630086b7bc9f3a03f79775eb48a1c31b52b74f
joy935 <tupaiv@gmail.com>
2024-10-12 16:43:54 +0200

    removed doublon

--- src/js/search-results.js
+++ src/js/search-results.js
@@ -9,10 +9,43 @@ const services = new ExternalServices();
 
 async function getSearchResults() {
   try {
-    const searchResults = await services.getData(getParam("search"));
+    const searchResults = getParam("search");
 
     if (searchResults.length > 0) {
-      renderListWithTemplate(productCardTemplate, box, searchResults);
+      // array of the products by categories
+      const categories = ["tents", "hammocks", "sleeping-bags", "backpacks"];
+
+      // fetch data for all categories concurrently using Promise.all
+      const getData = categories.map((category) => services.getData(category));
+      const allData = await Promise.all(getData);
+
+      // flatten the products into a single array
+      const allResults = allData.flat();
+
+      // check if the search query matches the products
+      const matchedResults = allResults.filter(
+        (product) =>
+          // match the brand
+          product.Brand.Name.toLowerCase().includes(
+            searchResults.toLowerCase(),
+          ) ||
+          // match the category
+          product.Category.toLowerCase().includes(
+            searchResults.toLowerCase(),
+          ) ||
+          // match the product name
+          product.Name.toLowerCase().includes(searchResults.toLowerCase()) ||
+          // match the color
+          product.Colors?.some((color) =>
+            color.ColorName.toLowerCase().includes(searchResults.toLowerCase()),
+          ),
+      );
+
+      if (matchedResults.length > 0) {
+        renderListWithTemplate(productCardTemplate, box, matchedResults);
+      } else {
+        box.innerHTML = "<p>No results found.</p>";
+      }
     } else {
       box.innerHTML = "<p>No results found.</p>";
     }
